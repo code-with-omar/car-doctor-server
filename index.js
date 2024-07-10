e99823bc42eeff1513e335b2a2e3af58b90f1390
Omar Faruk <omarfaruk65142@gmail.com>
2024-07-10 14:20:27 +0600

    Find One data from database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 
-const { MongoClient, ServerApiVersion } = require('mongodb')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
@@ -35,7 +35,13 @@ async function run() {
             res.send(result)
         })
 
-
+        // find single service 
+        app.get('/services/:id',async(req,res)=>{
+            const id=req.params.id;
+            const query={_id: new ObjectId(id)}
+            const result=await serviceCollection.findOne(query)
+            res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
