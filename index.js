85b87552dc152f5d48070070bb2864fe51b4ad2a
Omar Faruk <omarfaruk65142@gmail.com>
2024-07-17 17:04:08 +0600

    Create order API

--- index.js
+++ index.js
@@ -125,6 +125,12 @@ async function run() {
             const result = await productCollection.findOne(query)
             res.send(result)
         })
+        // product order api
+        app.post('/productsOrder', async(req,res)=>{
+            const productsOrder = req.body;
+            const result = await productOrderCollection.insertOne(productsOrder)
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
