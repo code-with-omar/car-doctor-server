147cbb06a8f9dbe1cf68e3520418e65019017882
Omar Faruk <omarfaruk65142@gmail.com>
2024-07-28 11:14:49 +0600

    Create order product delete api

--- index.js
+++ index.js
@@ -141,6 +141,14 @@ async function run() {
             const result = await productOrderCollection.find(query).toArray();
             res.send(result)
         })
+
+         // delete products order
+         app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await productOrderCollection.deleteOne(query);
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
